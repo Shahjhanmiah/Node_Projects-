4d73ea91a77bc39408329e5bf4e1f28aaa0c3682
mdshahjhan <mdshahjha478877@gmail.com>
2023-12-08 00:16:51 +0600

    developer

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,34 +1,79 @@
-import  { useState } from 'react';
-import axios from 'axios';
+import { Link } from "react-router-dom";
+import logo from '../assets/Tablet login-bro.png'
+import Swal from "sweetalert2";
+import { useContext } from "react";
+import { AuthContext } from "./AuthProvider";
+
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
 
-  const handleLogin = async () => {
-    try {
-      const response = await axios.post('http://localhost:3001/api/login', {
-        username,
-        password,
-      });
-      console.log(response.data);
-    } catch (error) {
-      console.error(error.response.data);
-    }
-  };
+  const { login } = useContext(AuthContext)
 
+  const handleLogin = event => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+
+
+    login(email, password)
+      .then(result => {
+
+        console.log(result.user)
+
+      })
+    // Swal and fire 
+    Swal.fire({
+      title: 'Success!',
+      text: 'User login Successfully',
+      icon: 'success',
+      confirmButtonText: 'Confirm'
+    })
+
+
+      .catch(error => console.log(error))
+  }
   return (
     <div>
-      <h2>User Login</h2>
-      <label>Username:</label>
-      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      <br />
-      <label>Password:</label>
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <br />
-      <button onClick={handleLogin}>Login</button>
+
+      <div>
+        <div className="hero w-full my-20">
+          <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row">
+            <div className="text-center lg:text-left">
+              <img className='w-3/4' src={logo} alt="" />
+            </div>
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
+              <h1 className="text-5xl text-center font-bold">Sign Up</h1>
+              <form onSubmit={handleLogin} className="card-body">
+
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input type="text" name='email' placeholder="email" className="input input-bordered" required />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+
+                </div>
+                <div className="form-control mt-6">
+                  <input className="btn btn-primary" type="submit" value="Login" />
+                </div>
+              </form>
+
+              <p className='text-center'>Already have an account? <Link className='text-blue-600 font-bold' to="/Register">Register</Link> </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
+
+
 
 export default Login;
